b0c70074f512017c53ccceafcaf5efac2c7dbfcc
AdrianFeijoFagundes <134751683+AdrianFeijoFagundes@users.noreply.github.com>
2024-07-01 18:08:15 -0300

    Add files via upload

---
+++ src/index.ts
@@ -0,0 +1,120 @@
+import * as readLineSync from 'readline-sync';
+
+import { Product } from "./classes/Product";
+import { Sell } from './classes/Sell';
+
+const item1 = new Product(1, 'Realme 7', 'Realme', 700, 1500, 70, 'Smartphone');
+const item2 = new Product(2, 'Controle de PS5', 'Sony', 100, 200, 200, 'Periféricos');
+const item3 = new Product(3, 'Fones de ouvido', 'Genérico', 15, 30, 300, 'Fones');
+
+const allProducts: Array<Product> = [];
+allProducts.push(item1); 
+allProducts.push(item2); 
+allProducts.push(item3); 
+const totalSells: Array<Sell> = [];
+
+function printProducts() {
+    for (let i = 0; i < allProducts.length; i++) {
+        console.log(allProducts[i].getProduct())
+    }
+}
+function main() : void {
+    let menu : string = '';
+
+    while (menu != 'N' && menu != 'n') {
+        console.log(" ========= Menu =========\n 1. Cadastrar novo Produto\n 2. Ver produtos\n 3. Realizar Venda\n 4. Ver vendas\n ========================\n")
+
+        console.log(" Escolha uma opção")
+        const option = readLineSync.questionInt(); 
+        console.clear()
+        switch (option) {
+            case 1:
+            
+                break
+            case 2:
+                printProducts()
+                break
+            case 3:
+                const id = totalSells.length + 1 
+                const date = new Date().toLocaleString('pt-BR', { timeZone: 'America/Sao_Paulo'})
+                let discountValue: number = 0
+                let discountPercentage: number = 0
+
+                const client = readLineSync.question('Qual o nome do cliente? ')
+                console.clear()
+
+                printProducts()
+                const chosenProduct = readLineSync.questionInt('\nQual produto ele deseja comprar? ') -1
+                const product = allProducts[chosenProduct]
+                console.clear()
+
+                const amout = readLineSync.questionInt('Qual a quantidade? ' )
+                console.clear()
+
+                const sell = new Sell(id, client, product, amout, date);
+
+                console.log("Vai ser aplicado desconto nessa venda?\n 1. Sim\n 2. Não")
+                const optionDiscount = readLineSync.questionInt()
+                console.clear()
+
+                if(optionDiscount === 1) {
+                    console.log("O desconto é em:\n 1. Valor fixo\n 2. Porcentagem\n 3. Porcentagem + Valor fixo")
+                    const discountMethod = readLineSync.questionInt()
+                    console.clear()
+                    switch (discountMethod) {    
+                        case 1:
+                            console.log("Qual o valor do desconto?")
+                            discountValue = readLineSync.questionInt()
+                            console.clear()
+                            break
+                        case 2:
+                            console.log("Qual a porcentagem do desconto?")
+                            discountPercentage = readLineSync.questionInt()
+                            console.clear()
+                            break
+                        case 3:
+                            console.log("Qual a porcentagem do desconto?")
+                            discountPercentage = readLineSync.questionInt()
+                            console.clear()
+                            console.log("Qual o valor do desconto?")
+                            discountValue = readLineSync.questionInt()
+                            console.clear()
+                            break
+                    }
+                }
+         
+                if (optionDiscount === 1) {
+                    sell.setDiscountValues(discountValue, discountPercentage)
+                }
+               
+                console.log("Total  sem descontos: R$" + sell.calcTotal())
+                console.log("Descontos: R$" + sell.calcDiscont())
+                console.log("Preço final: R$" + sell.calcFinalPrice())                
+
+                console.log("\n Qual o método de pagamento? \n 1.PIX\n 2.Crédito\n 3.Débito\n 4.Dinheiro\n")
+                const chosenMethod = readLineSync.questionInt() - 1;
+                const paymentMethodsList: Array<string> = ['PIX','Crédito','Débito','Dinheiro']
+                const paymentMethod = paymentMethodsList[chosenMethod]
+                console.clear()
+
+                sell.setPayment(paymentMethod)
+
+                totalSells.push(sell);
+                break
+            case 4:
+                for (let i = 0; i < totalSells.length; i++) {
+                    console.log(totalSells[i].getSell())
+                }
+                break
+            
+
+        }
+        console.log("\n Deseja continuar usando o programa? (S/N)")
+        menu = readLineSync.question();
+        console.clear()
+    }
+}
+
+main()
+
+// \n 3. Atualizar Produto
